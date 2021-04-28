0b17f1f8ac0b1d517c58add8f8c02c6dc8cfb442
Maxwell Lee <maxwell.lee@revature.net>
2021-04-28 12:59:37 -0700

    initial commit

---
+++ src/main/java/com/revature/oracles/maximuscarrental/MaximuscarrentalApplication.java
@@ -0,0 +1,13 @@
+package com.revature.oracles.maximuscarrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximuscarrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaximuscarrentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/oracles/maximuscarrental/MaximuscarrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.oracles.maximuscarrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaximuscarrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
