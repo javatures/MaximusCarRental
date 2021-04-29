9a982bd775062e57cf84cc10f6191fbc56220f35
Matthew Freeman <matthew.freeman@revature.net>
2021-04-29 10:27:51 -0400

    Changes file name of Home to SiginForm and fixes import statement at main App level

--- rental-react/src/App.tsx
+++ rental-react/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SigninForm from './Components/Home'
+import SigninForm from './Components/SigninForm'
 
 function App() {
   return (
