5d147886f4c64694259386defb1c664730d4e6b5
Charles Radcliffe <charles.radcliffe@revature.net>
2021-05-05 19:45:37 -0700

    Reservation form Layout Finished Next: Complete Reservation send to DB

---
+++ rental-react/src/components/DisplayAvailableCars.tsx
@@ -0,0 +1,58 @@
+import React, { FC, useEffect, useState } from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import CarHomepage from './CarHomepage';
+import CarCard from './CarCard';
+
+interface Car {
+    id: number;
+    make: string;
+    model: string;
+    type: string;
+    year: string;
+    renterId: number;
+}
+
+
+const DisplayAvailable: FC<{type: string}> = (props): JSX.Element =>{
+    const [cars, setCars] = useState(Array<Car>());
+
+    useEffect(() => {
+        fetch('http://localhost:8080/car')
+        .then(response => response.json())
+        .then(json => {
+            setCars(json);
+        })
+    }, [])
+
+    let carCards = [];
+    let newCols = [];
+    for(let i = 1; i <= cars.length; i++)
+    {   
+        let car = cars[i-1];
+        if(props.type === car.type ){
+            newCols.push(<Col key={i-1}><CarCard key={i-1} car={cars[i-1]}/></Col>)
+            if(i % 3 === 0)
+            {
+                carCards.push(<Row className="pb-2">{newCols}</Row>)
+                newCols = [];
+            }
+        }
+    }
+
+    if(newCols.length !== 0)
+    {
+        carCards.push(<Row className="pb-2">{newCols}</Row>)
+    }
+
+    return (
+        <Container>
+            <h3>Available {props.type} Vehicles</h3>
+            {carCards}
+        </Container>
+    )
+}
+
+
+export default DisplayAvailable
--- rental-react/src/components/ReservationForm.tsx
+++ rental-react/src/components/ReservationForm.tsx
@@ -1,8 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState, Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Container';
+import DisplayAvailable from './DisplayAvailableCars';
+import CurrentUser from './CurrentUser';
+import CarHomepage from './CarHomepage';
+import {Tab , Tabs, TabList, TabPanel} from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const ReservationForm: FC<{}> = (): JSX.Element => {
     const [pickupdate, setPickupdate] = React.useState("");
@@ -15,21 +19,19 @@ const ReservationForm: FC<{}> = (): JSX.Element => {
     return (
         <Form data-testid="reservationform">
             <Row>
-            <Col>
+            
                 <Form.Group controlId="pickupdata">
                     <Form.Label>Pick Up Date</Form.Label>
-                    <Form.Control type="date" value={pickupdate} onChange={(e) => setPickupdate(e.target.value)}/>
+                    <Form.Control required type="date" value={pickupdate} onChange={(e) => setPickupdate(e.target.value)}/>
                 </Form.Group>
-            </Col>
-            <Col>
+            
                 <Form.Group controlId="dropoffdate">
                     <Form.Label>Enter Drop Off Date</Form.Label>
-                    <Form.Control type="date" value={dropoffdate} onChange={(e) => setDropoffdate(e.target.value)}/>
+                    <Form.Control required type="date" value={dropoffdate} onChange={(e) => setDropoffdate(e.target.value)}/>
                 </Form.Group>
-            </Col>
             </Row>
             <Row>
-                <Form.Group id="cartype">
+                {/* <Form.Group id="cartype">
                     <Form.Label>Select Vehicle Type</Form.Label>
                     <Form.Control as="select" onChange={(e) => setCartype(e.target.value)}>
                         <option value="Select">Select...</option>
@@ -38,7 +40,34 @@ const ReservationForm: FC<{}> = (): JSX.Element => {
                         <option value="Sport">Sport</option>
                         <option value="Towing and Hauling">Towing and Hauling</option>
                     </Form.Control>
-                </Form.Group>
+                </Form.Group> */}
+            </Row>
+            <Row>
+            <Tabs>
+                    <TabList>
+                        <Tab>All</Tab>
+                        <Tab>Ecomony</Tab>
+                        <Tab>Luxury</Tab>
+                        <Tab>Sport</Tab>
+                        <Tab>Towing and Hauling</Tab>
+                    </TabList>
+                    <TabPanel>
+                        <h3>All Vehicles</h3>
+                        <CarHomepage />
+                    </TabPanel>
+                    <TabPanel>
+                        <DisplayAvailable type="Economy" />
+                    </TabPanel>
+                    <TabPanel>
+                        <DisplayAvailable type="Luxury" />
+                    </TabPanel>
+                    <TabPanel>
+                        <DisplayAvailable type="Sport" />
+                    </TabPanel>
+                    <TabPanel>
+                        <DisplayAvailable type="Towing and Hauling" />
+                    </TabPanel>
+                </Tabs>
 
             </Row>
         </Form>
